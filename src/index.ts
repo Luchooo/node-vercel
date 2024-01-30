1dfbb88778d177839eaa9f51dc93adfecaa37d96
Luis Sanchez <luis.udec.sanchez@hotmail.com>
2024-01-29 23:29:33 -0500

    feat: "Add morgan as middleware http"

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,21 @@
+import express from 'express'
+import morgan from 'morgan'
 import { print } from './utils/logger'
 
-print.info('luis')
-print.error('jackelin')
-print.warn('jackelin')
+const app = express()
+
+app.use(morgan('dev'))
+
+app.get('/', (_req, res) => {
+  res.send('Hello Luis')
+})
+
+app.use(function (_req, res) {
+  res.status(404).send('Sorry cant find that!')
+})
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+  print.info(`Server running on: http://localhost:${PORT}`)
+})
